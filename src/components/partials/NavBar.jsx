00ef11c7eb091cff0cb7cc38d5929611c2057476
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-20 16:01:15 +0100

    ADD navbar component

---
+++ src/components/partials/NavBar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login">Login</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/register">Register</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/not-found">not-found</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/update-password">Update Password</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar;
