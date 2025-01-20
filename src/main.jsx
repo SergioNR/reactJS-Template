cd5502dfd95317a37a60a947390922f4d0a6a97a
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-20 16:01:58 +0100

    ADD routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-
+import { BrowserRouter, Routes, Route } from 'react-router'
+import PageNotFound from './pages/404.jsx'
 import './index.css'
 import App from './App.jsx'
-
+import Login from './pages/login.jsx'
+import Register from './pages/register.jsx'
+import UpdatePassword from './pages/updatePassword.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+
+
+        <Route path="login" element={<Login />}>
+          <Route path="settings" element={<Register />} />
+        </Route>
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/update-password" element={<UpdatePassword />} />
+
+      <Route path="*" element={<PageNotFound />} />
+      </Routes>
+
     </BrowserRouter>
   </StrictMode>,
 )
