1589b7acf63dbaa70bac4d1c17063351dd060b34
Sergio Navarro <s.navarroredondo@gmail.com>
2025-02-24 12:17:58 +0100

    REFACTOR loginForm into its own file

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,12 @@
 import NavBar from '../components/partials/navBar'
+import LoginForm from '../components/LoginForm';
 
 const LoginPage = () => {
     return (
         <>
             <NavBar />
-            <div>
-                <h1>Login Page</h1>
-            <form method="post" action="/login">
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </form>
-        </div>
+            
+            <LoginForm />
         </>
     )
 };
