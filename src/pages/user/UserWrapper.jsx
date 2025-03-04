b6179d0a02fc162c2bcc441f3a4d8b9d33d86335
Sergio Navarro <s.navarroredondo@gmail.com>
2025-03-04 23:54:36 +0100

    ADD logic to check if user is logged in /user/

--- src/pages/user/UserWrapper.jsx
+++ src/pages/user/UserWrapper.jsx
@@ -1,10 +1,42 @@
 import { Outlet } from "react-router";
 import UserNavBar from "../../components/partials/UserNavBar";
 import UserFooter from "../../components/partials/UserFooter";
+import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const UserWrapper = () => {
 
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                await axios.get(`${import.meta.env.VITE_SERVER_API}/api/v1/auth/check-session`, {
+                    withCredentials: true
+                });
+                
+                setLoading(false);
+
+
+            } catch (error) {
+                console.error('Authentication check failed:', error);
+                navigate('/auth/login');
+            }
+        };
+
+        checkAuth();
+    }, [navigate]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
+        
+        
+        
         <div>
             <UserNavBar /> {/* Add UserNavBar component here */}
             <h1>This is the user profile wrapper, which contains different outlets</h1>
